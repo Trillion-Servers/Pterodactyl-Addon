27f56775f74a3ddbb75ba4341249b975af5db6ba
Griffindor301 <griffin.hoover2@outlook.com>
2020-07-29 10:07:05 -0400

    Updated Regular Bot Files to Supportbot 6.0

--- node_modules/nodeactyl/application/methods/getAllUsers.js
+++
@@ -1,8 +0,0 @@
-let req = require('../ApplicationRequest.js');
-
-function getAllUsers() {
-    const Req = new req(process.env.APPLICATION_NODEACTYL_HOST, process.env.APPLICATION_NODEACTYL_KEY);
-    return Req.getRequest("GetAllUsers", null);
-}
-
-module.exports = getAllUsers;
